d6313bff78139b569046f55731655c978dd96809
pahopelt@luukku.com <pahopelt@luukku.com@315b9fec-d1f4-11dd-86d7-074df07e0730>
2009-01-17 17:51:49 +0000

    Made control field conversion to unimarc_text lossless, which also solved issue 2

--- trunk/src/main/java/ppa/marc/converter/generic/FieldIdChanger.java
+++ trunk/src/main/java/ppa/marc/converter/generic/FieldIdChanger.java
@@ -8,23 +8,29 @@ public class FieldIdChanger implements FieldConverter {
 	private final int newId;
 	private final Character newFirstIndicator;
 	private final Character newSecondIndicator;
-
+	private final boolean isControlField;
+	
 	public FieldIdChanger(int newId) {
 		this.newId = newId;
 		this.newFirstIndicator = null;
 		this.newSecondIndicator = null;
+		this.isControlField = true;
 	}
 
 	public FieldIdChanger(int newId, char newFirstIndicator, char newSecondIndicator) {
 		this.newId = newId;
 		this.newFirstIndicator = newFirstIndicator;
 		this.newSecondIndicator = newSecondIndicator;
+		this.isControlField = false;
 	}
 
 	public void convert(Field field) {
 		field.setId(newId);
-		if(newFirstIndicator != null) field.setFirstIndicator(newFirstIndicator);
-		if(newSecondIndicator != null) field.setSecondIndicator(newSecondIndicator);
+		if(!isControlField) {
+			field.setFirstIndicator(newFirstIndicator);
+			field.setSecondIndicator(newSecondIndicator);
+		}
+		field.setIsControlField(isControlField);
 	}
 
 }
